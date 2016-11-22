3dc6f231bacbe268633a8fec10600c861a42bf6e
Polyakov Igor <polykovio@mail.ru>
2016-11-22 22:47:29 +0300

    Reset project type for server

---
+++ TicTacToeServer/TicTacToeServer/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="TicTacToeServer.Global" Language="C#" %>
---
+++ TicTacToeServer/TicTacToeServer/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(TicTacToeServer.Startup))]
+namespace TicTacToeServer
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
